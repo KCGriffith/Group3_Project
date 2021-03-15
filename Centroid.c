f4740c481ccc7093a4667f79dce13e8a796953c8
ganesh050 <ganeshsharma@my.smccd.edu>
2021-03-15 16:42:19 -0500

    changes

--- Centroid.c
+++ Centroid.c
@@ -11,45 +11,33 @@ struct point
 
 struct Cluster
 {
-    point clu [100];
+    //size of cluster
+    int n;
+    point clu [n];
 };
 
 struct Centroid
 {
-    point cen [100];
+    point cen [100];  //n determines the size of this array.
 }
 //generates centroid
-point *Centroid(point * cluster, int num_data_points)
+point *Centroid(cluster * cluster, int num_data_points)
 {
     int size, a;
     scanf ("%i", &size);
     double centroid [size];
     int num_data_points, num_clusters;
 
-    //initalization of data points x , y and cluster
-    for (int i = 0 ; i < num_data_points ; i++)
-    {
-        centroid[i].num = 0 ;
-        centroid[i].x = 0;
-        centroid[i].y = 0;  
-    }
-    //call random cluster function that assigns data points to random cluster
-    randomCluster();
-
     //dividing the cluster's data points by the number of data points in the cluster
     for (int i = 0; i < num_clusters; i++;)
     {
-        centroid[i].x = centroids[i].x / centroids[i].num;
-        centroid[i].y = centroids[i].y / centroids[i].num;   
-    }  
-    
-    //put nearest data points to one cluster
-    for (int i = 0 ; i < num_points; i++)
-    {
         
-    }
-
-
+        Centroid.x += cluster[i].x; 
+        Centroid.y += cluster[i].y;
+    } 
+    centroid.x /= cluster.n; 
+    centroid.y /= cluster.n;
+    
     return centroid;
 
 };
