696fecc04cf64397740eea7b9fe950bb63545edd
SammieMinniefield4 <80222025+SammieMinniefield4@users.noreply.github.com>
2021-03-15 17:52:15 -0500

    Update tightenCluster.c

--- tightenCluster.c
+++ tightenCluster.c
@@ -1,5 +1,87 @@
 #include <stdio.h>
 
+using namespace std;
+
+struct point{  //Place holder for datastructure.
+	/*centroids are double values so I feel
+	  it would be simpiler to record all points
+	  as doubles rather than undergo in conversions
+	  Also I haven't looked at the breast cancer dataset
+	  and there might be double values there as well
+	  so this is just future proofing on multiple fronts.*/
+	int x;
+    int y;
+    double x;
+	double y;
+    num;
+}
+
+struct cluster{
+	/*This will act as a list of points.
+	  It will also keep track of how many
+	  points are in a cluster.
+	  */
+	int n; //Number of elements determined during randCluster and tightenCluster.
+	point cluster[n];
+    int a;
+    point cluster[a];
+}
+
+//Function to reassign elements
+cluster[] tighten_cluster(cluster[] C);
+
+int main(void){
+	//For the time being ignore the fact C has to be initialized as a list.
+	//At least until the data structure is in place.
+	int k, n = /*Input data's size*/;
+	point D[n] = [initPoint(/*Input data*/)*n];
+	cluster result;
+
+	printf("Input number of clusters: ");
+	scanf("%d", &k);
+	printf("\n");
+
+	result = kmeans(D, k);
+
+	printf(/*We need to workout how to output results*/);
+
+	return 0;
+
+}
+
+cluster[] kmeans(point[] D, int k){
+	//Initializaition
+	cluster[] C = rand_Cluster(D, k);
+	point centroids[k] = get_centroid(C);
+
+	//Book Keeping
+	point old_centroids[k] = void;
+
+	while(!should_stop(old_centroids, centorids)){
+
+		old_centroids = controids;
+
+		//Reassign objects to the cluster.
+
+		C = tighten_cluster(C);
+
+		//Update the cluster means
+
+		centroids = get_centroid(C);
+
+	}
+	return C;
+}
+
+//Random Cluster function
+cluster[] rand_Cluster(point[] D, int k){}
+
+//Check to end kmean function.
+bool should_stop(point[k] old_centroids, point[k] centroids){
+	if(old_centroids == void) return false;
+	if(old_centroids == centroids) return true;  //== operator needs an override but it depends on the data structure we use.
+	return false;
+}
 public void Cluster()
     {
       // init clustering[] and means[][] 
