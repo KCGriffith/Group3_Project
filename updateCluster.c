4b724741685770248eb50fda4abfd3d2f5ae3231
KCGriffith <keatongriff@gmail.com>
2021-03-18 18:30:10 -0500

    Change to updateCluster that sets up for testing.
    Requires implementaition of functions for E_calculator

--- updateCluster.c
+++ updateCluster.c
@@ -3,6 +3,26 @@
 #include <math.h>
 #incluce <stdbool.h>
 
+typedef struct point{  //Place holder for datastructure.
+	/*centroids are double values so I feel
+	  it would be simpiler to record all points
+	  as doubles rather than undergo in conversions
+	  Also I haven't looked at the breast cancer dataset
+	  and there might be double values there as well
+	  so this is just future proofing on multiple fronts.*/
+	double x;
+	double y;
+} Point;  //(x, y) <-- That is a point.
+
+typedef struct cluster{
+	/*This will act as a list of points.
+	  It will also keep track of how many
+	  points are in a cluster.
+	  */
+	int n; //Number of elements determined during randCluster and tightenCluster.
+	struct point *c;
+} Cluster;  //A cluster is a list of points {(x,y), (x,y)} For example and keeps trak of how many data points it holds which is n.  In this example n = 2.
+
 float centroidPoint1(float aa, float bb)
 {
     float centroidx = aa;
@@ -76,9 +96,21 @@ struct point UpdateClustering(int clustering[],
 
       return true;  // successful change to clustering so keep looping
     }
+
+//point initializer
+struct point init_point(double x, double y){
+	return (Point) {x, y};
+}
+
+//cluster initializer I don't know what this is for.
+struct cluster init_cluster(struct point *D, int n){
+	return (Cluster) {n, D};
+}
+
 int main()
 {
     float result1,result2,result3,aa,bb, a, b, c, d, e, f;
+    
     printf("\nEnter The Coordinates of Centroid A:\n");
     printf("\nX - Axis Coordinate: \n");
     scanf("%f", &aa);
@@ -103,5 +135,26 @@ int main()
     result2 = distance_calculator2(aa, bb, c, d);
     result3 = distance_calculator3(aa, bb, e, f);
     printf("\nDistance between Points A, B, and C: %f %f %f \n", result1, result2, result3);
+    
+    int n = 8, k = 3, hm = 0; //hm keeps track of how many data point were assigned to a cluster.
+	double x[8] = {2, 2, 8, 5, 7, 6, 1, 4}; //These are test values meant to be turned into data points
+	double y[8] = {10, 5, 4, 8, 5, 4, 2, 9};
+    struct point *D = calloc(n, sizeof(Point));  //This will hold data points until we can distribute them into clusters.
+    struct cluster *C = calloc(k, sizeof(Cluster));
+
+	for(int i = 0; i < n; i++){
+		*(D+i) = init_point(x[i], y[i]);
+	}
+    
+    for(int i = 0; i < k; i++){
+        if(i == k-1){
+            *(C+i) = init_cluster(D, 4)
+        }
+        *(C+i) = init_cluster(D, 2)
+        D += 2 //This changes the address pointed to from (D+0) to (D+2) as an example.
+    }
+    
+    
+    
     return 0;
 }
