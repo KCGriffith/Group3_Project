d5552e86f5cc03a783380c11d5e75c7289e9429b
ganesh050 <ganeshsharma@my.smccd.edu>
2021-04-04 19:22:50 -0500

    update

--- kmeditest.c
+++ kmeditest.c
@@ -7,12 +7,11 @@
 #include "kmeansf.h"
 #include "medistruct.h"
 
-
 int main(int argc, char *argv[])
 {
 	//For the time being ignore the fact C has to be initialized as a list.
 	//At least until the data structure is in place.
-	int MAX_VALUE = 699;
+	int MAX_VALUE = 5;
 
 	FILE *inFile;
 	struct point *p = (Point*)calloc(MAX_VALUE, sizeof(Point));
@@ -21,11 +20,15 @@ int main(int argc, char *argv[])
 	Cluster* result = (Cluster*)calloc(2, sizeof(Cluster));
 	int k = 2;
 
+	printf("WE ARE CHECKING IF ALL POINTS WERE COLLECTED\n");
+    for(int i = 0; i < valid; i++){
+    	print_point((p+i));
+    	printf("\n");
+    }
+
 	struct point *D = (Point*)calloc(valid, sizeof(Point));
 	bool val = true;
 	int count = 0;
-
-	printf("valid: %d\n", valid);
 
 	for(int i = 0; i < valid; i++)
     {
@@ -49,8 +52,14 @@ int main(int argc, char *argv[])
         }
     }
 
+	printf("WE ARE CHECKING IF ALL POINTS WERE MINIMIZED\n");
+    for(int i = 0; i < valid; i++){
+    	print_point((D+i));
+    	printf("\n");
+    }
 
 	result = kmeans(D, valid, k);
+	
 	printf("This clustering accuracy = %lf\n", Accuracy(result));
 
 	return 0;
